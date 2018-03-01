015f9ef61066385459316dc4fddded2e3e63ffdd
Simone Celia <simon387@hotmail.it>
2018-03-01 17:08:27 +0100

    rmv_header

--- js/main.js
+++ js/main.js
@@ -10,129 +10,130 @@
  */
 jQuery(document).ready(function () {
 
-    // inner variables
-    var song;
-    var tracker = $('.tracker');
-    var volume = $('.volume');
+	// inner variables
+	var song;
+	var tracker = $('.tracker');
+	var volume = $('.volume');
 
-    function initAudio(elem) {
-        var url = elem.attr('audiourl');
-        var title = elem.text();
-        var cover = elem.attr('cover');
-        var artist = elem.attr('artist');
+	function initAudio(elem) {
+		var url = elem.attr('audiourl');
+		var title = elem.text();
+		var cover = elem.attr('cover');
+		var artist = elem.attr('artist');
 
-        $('.player .title').text(title);
-        $('.player .artist').text(artist);
-        $('.player .cover').css('background-image', 'url(data/' + cover + ')');;
+		$('.player .title').text(title);
+		$('.player .artist').text(artist);
+		$('.player .cover').css('background-image', 'url(data/' + cover + ')');;
 
-        song = new Audio('data/' + url);
+		song = new Audio('data/' + url);
 
-        // timeupdate event listener
-        song.addEventListener('timeupdate', function () {
-            var curtime = parseInt(song.currentTime, 10);
-            tracker.slider('value', curtime);
-        });
+		// timeupdate event listener
+		song.addEventListener('timeupdate', function () {
+			var curtime = parseInt(song.currentTime, 10);
+			tracker.slider('value', curtime);
+		});
 
-        $('.playlist li').removeClass('active');
-        elem.addClass('active');
-    }
+		$('.playlist li').removeClass('active');
+		elem.addClass('active');
+	}
 
-    function playAudio() {
-        song.play();
+	function playAudio() {
+		song.play();
 
-        tracker.slider("option", "max", song.duration);
+		tracker.slider("option", "max", song.duration);
 
-        $('.play').addClass('hidden');
-        $('.pause').addClass('visible');
-    }
-    
-    function stopAudio() {
-        song.pause();
+		$('.play').addClass('hidden');
+		$('.pause').addClass('visible');
+	}
 
-        $('.play').removeClass('hidden');
-        $('.pause').removeClass('visible');
-    }
+	function stopAudio() {
+		song.pause();
 
-    // play click
-    $('.play').click(function (e) {
-        e.preventDefault();
+		$('.play').removeClass('hidden');
+		$('.pause').removeClass('visible');
+	}
 
-        playAudio();
-    });
+	// play click
+	$('.play').click(function (e) {
+		e.preventDefault();
 
-    // pause click
-    $('.pause').click(function (e) {
-        e.preventDefault();
+		playAudio();
+	});
 
-        stopAudio();
-    });
+	// pause click
+	$('.pause').click(function (e) {
+		e.preventDefault();
 
-    // forward click
-    $('.fwd').click(function (e) {
-        e.preventDefault();
+		stopAudio();
+	});
 
-        stopAudio();
+	// forward click
+	$('.fwd').click(function (e) {
+		e.preventDefault();
 
-        var next = $('.playlist li.active').next();
-        if (next.length == 0) {
-            next = $('.playlist li:first-child');
-        }
-        initAudio(next);
-    });
+		stopAudio();
 
-    // rewind click
-    $('.rew').click(function (e) {
-        e.preventDefault();
+		var next = $('.playlist li.active').next();
+		if (next.length == 0) {
+			next = $('.playlist li:first-child');
+		}
+		initAudio(next);
+	});
 
-        stopAudio();
+	// rewind click
+	$('.rew').click(function (e) {
+		e.preventDefault();
 
-        var prev = $('.playlist li.active').prev();
-        if (prev.length == 0) {
-            prev = $('.playlist li:last-child');
-        }
-        initAudio(prev);
-    });
+		stopAudio();
 
-    // show playlist
-    $('.pl').click(function (e) {
-        e.preventDefault();
+		var prev = $('.playlist li.active').prev();
+		if (prev.length == 0) {
+			prev = $('.playlist li:last-child');
+		}
+		initAudio(prev);
+	});
 
-        $('.playlist').fadeIn(300);
-    });
+	// show playlist
+	$('.pl').click(function (e) {
+		e.preventDefault();
 
-    // playlist elements - click
-    $('.playlist li').click(function () {
-        stopAudio();
-        initAudio($(this));
-    });
+		$('.playlist').fadeIn(300);
+	});
 
-    // initialization - first element in playlist
-    initAudio($('.playlist li:first-child'));
+	// playlist elements - click
+	$('.playlist li').click(function () {
+		stopAudio();
+		initAudio($(this));
+	});
 
-    // set volume
-    song.volume = 0.8;
+	// initialization - first element in playlist
+	initAudio($('.playlist li:first-child'));
 
-    // initialize the volume slider
-    volume.slider({
-        range: 'min',
-        min: 1,
-        max: 100,
-        value: 80,
-        start: function (event, ui) { },
-        slide: function (event, ui) {
-            song.volume = ui.value / 100;
-        },
-        stop: function (event, ui) { },
-    });
+	// set volume
+	song.volume = 0.8;
 
-    // empty tracker slider
-    tracker.slider({
-        range: 'min',
-        min: 0, max: 10,
-        start: function (event, ui) { },
-        slide: function (event, ui) {
-            song.currentTime = ui.value;
-        },
-        stop: function (event, ui) { }
-    });
+	// initialize the volume slider
+	volume.slider({
+		range: 'min',
+		min: 1,
+		max: 100,
+		value: 80,
+		start: function (event, ui) {},
+		slide: function (event, ui) {
+			song.volume = ui.value / 100;
+		},
+		stop: function (event, ui) {},
+	});
+
+	// empty tracker slider
+	tracker.slider({
+		range: 'min',
+		min: 0,
+		max: 10,
+		start: function (event, ui) {},
+		slide: function (event, ui) {
+			song.currentTime = ui.value;
+		},
+		stop: function (event, ui) {}
+	});
 });
